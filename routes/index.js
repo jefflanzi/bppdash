227d5c477a3a7242b9ae4b6967cf2eb341c13d53
Jeff Lanzi <jefflanzi@gmail.com>
2015-11-25 13:27:00 -0800

    restructure routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Brand Performance Platform' });
+  res.render('index', { title: 'Brand Performance Platform' })
+});
+
+// GET any other views
+router.get('/*', function(req, res, next) {
+  filename = req.params[0]
+  res.render(filename, { title: 'Brand Performance Platform' })
 });
 
 module.exports = router;
