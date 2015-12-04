ac6f4238ecf98142f176f3b50670db30a62f1f6e
Jeff Lanzi <jefflanzi@gmail.com>
2015-12-03 16:18:46 -0800

    lines grouped by company

--- views/charts/attributeChart.js
+++ views/charts/attributeChart.js
@@ -8,6 +8,7 @@ function attributeChart(selection) {
 
   var xScale = d3.scale.linear().range([0, width]);
   var yScale = d3.scale.ordinal().rangeRoundBands([0, height]);
+  var color = d3.scale.category10();
 
   var chartArea = selection.append('svg')
     .attr({
@@ -19,37 +20,54 @@ function attributeChart(selection) {
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   d3.csv('/data/attributes.csv', function(data) {
-    dataset = data;
-    // var xMin = d3.min(data, function(d) {
-    //   return d3.min(d3.values(d).slice(2))
-    // });
-    // var xMax = d3.max(data, function(d) {
-    //   return d3.max(d3.values(d).slice(2))
-    // });
-
     // Scales
     var xValues = [];
-    for (i in data) {
-      xValues = xValues.concat(d3.values(data[i]).slice(2));
-    }
+    for (i in data) { xValues = xValues.concat(d3.values(data[i]).slice(2)); }
     xScale.domain(d3.extent(xValues));
 
     var yValues = [];
-    for (i in data) {
-      yValues.push(data[i].attribute)
-    }
-    console.log(yValues);
+    for (i in data) { yValues.push(data[i].attribute) };
     yScale.domain(yValues);
 
+    var companyNames = d3.keys(data[0]).filter(function(key) { return key !== 'attribute' & key !== 'order'});
+    color.domain(companyNames);
+
     // Lines
+    var companies = companyNames.map(function(name) {
+      return {
+        name: name,
+        values: data.map(function(d) {
+          return {attribute: d.attribute, rating: +d[name]};
+        })
+      };
+    });
+
     var line = d3.svg.line()
-      .x(function(d) { return xScale(d['Cornerstone OnDemand']) })
+      .x(function(d) { return xScale(d.rating) })
       .y(function(d) { return yScale(d.attribute) })
 
-    chartArea.append('path')
-      .datum(data)
+    var company = chartArea.selectAll('.company')
+      .data(companies)
+      .enter()
+      .append('g')
+      .attr('class', 'company');
+
+    company.append('path')
       .attr('class', 'line')
-      .attr('d', line)
+      .attr('d', function(d) { return line(d.values); })
+      .style('stroke', function(d) { return color(d.name); });
+
+    // Dots
+    // company.append('circle')
+    //   .attr({
+    //     cx: function(d) { xScale(d.values.rating) },
+    //     cy: function(d) { yScale(d.values.attribute) },
+    //     r: 5
+    //   });
+
+
+
+  // End d3.csv()
   });
 
 // End attributeChart()
