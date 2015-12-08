16517124c3476e182efcc7d7b3d548b708f03837
jeff <jefflanzi@gmail.com>
2015-12-07 23:08:57 -0800

    vertical responsive bar chart

--- views/charts/bar.js
+++ views/charts/bar.js
@@ -1,32 +1,29 @@
 // http://bost.ocks.org/mike/chart/
 function barChart(selection) {
+  //- Draw SVG
+  var margin = {top: 20, right: 20, bottom: 30, left: 140};
+  var width = parseInt(selection.style('width')) - margin.left - margin.right;
+  var height = parseInt(selection.style('height')) - margin.top - margin.bottom;
+
+  var xScale = d3.scale.linear()
+    .range([0, width]);
+
+  var yScale = d3.scale.ordinal()
+    .rangeRoundBands([0, height], 0.01);
+
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient('bottom');
+
+  var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient('left');
 
   function chart() {
-    //- Draw SVG
-    var margin = {top: 20, right: 20, bottom: 30, left: 140};
-    var width = parseInt(selection.style('width')) - margin.left - margin.right;
-    var height = parseInt(selection.style('height')) - margin.top - margin.bottom;
-
-    var xScale = d3.scale.linear()
-      .range([0, width]);
-
-    var yScale = d3.scale.ordinal()
-      .rangeRoundBands([0, height], 0.01);
-
-    var xAxis = d3.svg.axis()
-      .scale(xScale)
-      .orient('bottom');
-
-    var yAxis = d3.svg.axis()
-      .scale(yScale)
-      .orient('left');
-
-    var chart = selection.append('svg')
+    var chartArea = selection.append('svg')
       .attr({
         width: width + margin.left + margin.right,
         height: height + margin.top + margin.bottom,
-        // viewBox: '0 0 ' + width + ' ' + height,
-        // preserveAspectRatio: 'xMinYMin'
       })
       .append('g')
       .attr('id', 'chartArea')
@@ -34,24 +31,24 @@ function barChart(selection) {
 
     //- load data and call constructor functions
     d3.csv('/data/jobTitles.csv', function(error, data) {
-
       //- Scales
       var xMax = d3.max(data, function(d) { return +d.Percent });
       xScale.domain([0, xMax]);
       yScale.domain(data.map(function(d) { return d.Title }));
 
       //- Draw Axis
-      chart.append('g')
+      chartArea.append('g')
         .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(xAxis);
+        // .attr('transform', 'translate(0,' + height + ')')
+        // .call(xAxis);
 
-      chart.append('g')
+      chartArea.append('g')
         .attr('class', 'y axis')
-        .call(yAxis);
+        .attr('transform', 'translate(' + width + ', 0)')
+        // .call(yAxis);
 
       //- Create bar group elements
-      var bars = chart.selectAll('g.new')
+      var bars = chartArea.selectAll('g.bar')
         .data(data)
         .enter()
         .append('g')
@@ -59,12 +56,8 @@ function barChart(selection) {
 
       //- Draw bar rects
       bars.append('rect')
-        .attr({
-          x: xScale(0),
-          y: function(d) { return yScale(d.Title) },
-          width: 0,
-          height: yScale.rangeBand()
-        });
+        .attr('x', 0)
+        .attr('width', 0);
 
       //-  Draw bar value Labels
       var valuePad = .01;
@@ -79,45 +72,58 @@ function barChart(selection) {
           'text-anchor' : 'end'
         });
 
-      resize(1000);
-      d3.select(window).on('resize', function() { resize(500); });
+      chart.resize(1000);
+      d3.select(window).on('resize', function() { chart.resize(500); });
 
-    //- End d3.csv() function
+    //- End d3.csv()
     });
+  //- End chart.draw()
+  }
 
-    //- responsive resize and animations
-    function resize(duration) {
+  //- responsive resize and animations
+  chart.resize = function resize(duration) {
 
-      var duration = duration || 500;
+    var duration = duration || 500;
 
-      width = parseInt(selection.style('width')) - margin.left - margin.right;
-      xScale.range([0, width]);
+    width = parseInt(selection.style('width')) - margin.left - margin.right;
+    height = parseInt(selection.style('height')) - margin.top - margin.bottom;
+    xScale.range([0, width]);
+    yScale.rangeRoundBands([0, height], 0.01);
 
-      d3.select('#chart svg')
-        .transition()
-        .duration(duration)
-        .attr('width', width + margin.left + margin.right + 'px');
+    d3.select('#chart svg')
+      .transition()
+      .duration(duration)
+      .attr('width', width + margin.left + margin.right + 'px')
+      .attr('height', height + margin.top + margin.bottom + 'px');
 
-      d3.selectAll('.bar rect')
-        .transition()
-        .duration(duration)
-        .attr('width', function(d) { return xScale(+d.Percent) })
+    d3.selectAll('.bar rect')
+      .transition()
+      .duration(duration)
+      .attr('width', function(d) { return xScale(+d.Percent) })
+      .attr('y', function(d) { return yScale(d.Title) })
+      .attr('height', yScale.rangeBand());
 
-      d3.selectAll('.valueLabel')
-        .transition()
-        .duration(duration)
-        .attr('x', function(d) { return xScale(+d.Percent) })
+    d3.selectAll('.valueLabel')
+      .transition()
+      .duration(duration)
+      .attr('x', function(d) { return xScale(+d.Percent) })
+      .attr('y', function(d) { return yScale(d.Title) + yScale.rangeBand() / 2 });
 
-      d3.select('.x.axis')
-        .transition()
-        .duration(duration * 0.5)
-        .call(xAxis);
-      // d3.select('.y.axis').call(yAxis);
-    }
+    d3.select('.x.axis')
+      .transition()
+      .duration(duration)
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(xAxis)
 
-  //- End function chart()
+    d3.select('.y.axis')
+      .transition()
+      .duration(duration)
+      .attr('transform', 'translate(' + 0 + ', 0)')
+      .call(yAxis);
+
+  // End chart.resize();
   }
 
   return chart;
-  //- End function barChart()
-  };
+//- End barChart()
+};
