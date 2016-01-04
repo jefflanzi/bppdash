64117ac84cac1c62d8b02af39edc802af9e2d232
jeff <jefflanzi@gmail.com>
2016-01-03 22:53:19 -0800

    setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 
+gulp.task('css', function() {
+  
+}
 gulp.task('default', function() {
   nodemon({
     script: './bin/www'
