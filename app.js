5887a3abba5cbd62be0d8e00e0d4c479b5736aa9
jeff <jefflanzi@gmail.com>
2016-01-05 23:16:04 -0800

    basic user model

--- app.js
+++ app.js
@@ -4,17 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('./config/mongoose');
 
 var app = express();
 
-// view engine setup
+// jade setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// Middlewares
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,7 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+var routes = require('./routes/index');
 app.use('/', routes);
+var users = require('./routes/users');
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find(function (err, users) {
+    if (err) return console.error(err);
+    res.send(users);
+  })
+});
+
+// GET a single user by ID
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id, function(err, data) {
+    if(err) return console.error(err);
+    res.send(data);
+  });
+});
+
+// create user
+router.post('/', function(req, res) {
+  var user = new User(req.body);
+  user.save(function (err) {
+    if (err) return console.error(err);
+  });
+
+  res.json(user);
 });
 
 module.exports = router;
