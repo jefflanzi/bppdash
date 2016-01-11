c42bddd45ba4f7450ce08b296194cca67a62bf96
jeff <jefflanzi@gmail.com>
2016-01-10 23:37:48 -0800

    use mongodb to store sessions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//==============================================================================
+// Dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,11 +7,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var mongoose = require('./config/mongoose');
+var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-// var MongoStore = require('connect-mongo')(session);
+var MongoStore = require('connect-mongo')(session);
 
+//==============================================================================
+// Express app setup
 var app = express();
 
 // jade setup
@@ -24,11 +28,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//==============================================================================
+// Mongoose
+var envURIs = {
+  development: 'mongodb://localhost/bppdash',
+  test: 'mongodb://localhost/bppdash-test'
+}
+var mongoURI = envURIs[app.get('env')];
+console.log('connecting to : ' + mongoURI);
+mongoose.connect(mongoURI);
+
+// check for successful connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongodb connected to ' + mongoURI);
+});
+
+//==============================================================================
 // Passport middleware
 app.use(session({
   resave: false,
   saveUninitialized: false,
   secret: 'secret',
+  // store: new MongoStore({ url: 'mongodb://localhost/bppdash-sessions' })
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,6 +63,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//==============================================================================
 // Routes
 var routes = require('./routes/routes');
 app.use('/', routes);
@@ -50,6 +75,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+//==============================================================================
 // error handlers
 
 // development error handler
@@ -74,4 +100,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//==============================================================================
+// Export app
 module.exports = app;
