3b10e8b7a057ef060650b8e52c1469d2411381ee
jeff <jefflanzi@gmail.com>
2016-01-10 23:09:04 -0800

    basic authentication working

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var mongoose = require('./config/mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+// var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -12,23 +16,32 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Middlewares
+// Express Middlewares
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Passport middleware
-app.use(express.session());
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret',
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Passport config
+var User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // Routes
-var routes = require('./routes/index');
+var routes = require('./routes/routes');
 app.use('/', routes);
-var users = require('./routes/users');
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/routes.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var User = require('../models/user.js');
+
+// Authentication routes
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.post('/login',  passport.authenticate('local'),  function(req, res) {
+  res.redirect('/');
+});
+
+// Need separate admin authentication to protect user registration
+router.get('/register', function(req, res) {
+  res.render('register')
+});
+
+router.post('/register', function(req, res) {
+  User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
+    if (err) {
+      return res.render('register', {user: user});
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/');
+    });
+  });
+});
+
+// All routes after this will check for successful authentication
+router.all('*', function(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+});
+
+router.get('/', function(req, res) {
+  res.render('index', { user: req.user });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/login');
+});
+
+// Chart views
+router.get('/charts/*', function(req, res, next) {
+  filename = 'charts/' + req.params[0]
+  res.render(filename, { title: 'Brand Performance Platform' })
+});
+
+router.get('/roadmap', function(req, res, next) {
+  res.render('roadmap');
+});
+
+module.exports = router;
