30ba08cd565b076a0a49cf9bcdcb4b06ebd21ff8
Jeff Lanzi <jefflanzi@gmail.com>
2016-02-17 15:07:34 -0800

    implement basic working upload form #8

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
 var fs = require('fs');
 
-var username = 'skillsoft';
+//==============================================================================
+// Configure file upload middleware
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+      var path = './datasets/' + req.user.username;
+      cb(null, path);
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
 
+var upload = multer({storage: storage});
+
+//==============================================================================
+// Define routes
+
+// Return a file with filename matching the model parameter
 router.get('/model/:model', function(req, res) {
   console.log(req.params.model);
   var ext = req.params.model === 'marketMap' ? '.json' : '.csv';
@@ -13,16 +30,16 @@ router.get('/model/:model', function(req, res) {
   });
 });
 
+// Render file upload page
 router.get('/upload', function(req, res) {
   res.render('upload', {user: req.user});
 });
 
-router.post('/upload', function(req, res) {
-  var path = './datasets/' + req.user.username + '/' + req.params.file.filename;
-  fs.write(path, data, function(err) {
-    if (err) throw err;
-    res.send('Upload successful');
-  });
+// Upload file to user's data folder
+router.post('/upload', upload.single('file'), function(req, res) {
+  console.log(req.file);
+  res.send(req.file);
+  // res.send('file uploaded to ' + req.file.path);
 });
 
 module.exports = router;
