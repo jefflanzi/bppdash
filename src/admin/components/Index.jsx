c902baf02baf3c230b0f965d7327158c4b341932
Jeff Lanzi <jefflanzi@gmail.com>
2016-03-03 14:09:40 -0800

    abstract getUsers to a component method new default admin index

--- src/admin/components/Index.jsx
+++ src/admin/components/Index.jsx
@@ -1,81 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SelectField, TextField, MenuItem, RaisedButton, Paper, Card} from 'material-ui';
-import FileInput from './FileInput';
-
-// Mock user data
-const users = [
-  { id: 1, username: 'User 1' },
-  { id: 2, username: 'User 2' },
-  { id: 3, username: 'User 3' }
-];
-
-const surveys = [
-  { id: 11, surveyname: 'Survey 1' },
-  { id: 12, surveyname: 'Survey 2' },
-  { id: 13, surveyname: 'Survey 3' }
-];
-
-const charts = [
-  { type: 'SalesFunnel', label: 'Sales Funnel' },
-  { type: 'Attributes', label: 'Attributes' },
-  { type: 'MarketMap', label: 'MarketMap' },
-  { type: 'BPI', label: 'Brand Performance Index' }
-];
+import {Paper} from 'material-ui';
 
 export default class Charts extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        username: '',
-        survey: '',
-        chart: '',
-        file: 'none'
-      }
-    };
-  }
-
-  handleChange = (field, event, index, value) => {
-    let data = this.state.data;
-    data[field] = value;
-    this.setState({data: data});
-  }
-
-  handleUpload = (event, index, value) => {
-    var button = ReactDOM.findDOMNode(this.refs.fileInput);
-    button.click();
-  }
-
-  handleTest = event => {
-    let filename = event.target.files[0].name;
-    let data = this.state.data;
-    data.file = filename;
-    this.setState({data: data});
-  }
 
   render() {
 
     return (
       <Paper style={{padding: 1.5 + 'rem', margin: 1 + 'rem'}}>
-        <h2>Upload Test</h2>
-        <form role="form" action="/data/upload" method="post" encType="multipart/form-data">
-          <input name="username" type="text" value="test"/>
-          <br />
-          <input name="survey" type="text" value="test"/>
-          <br />          
-          <input name="chart" type="text" value="test"/>
-          <br />
-          <input name="file" ref="fileInput" type="file" />
-          <br />
-          <RaisedButton
-            label="Upload"
-            type="submit"
-            primary={true}
-            style={{marginTop: 25}}
-          />
-        </form>
+        <h2>Welcome to the Admin UI</h2>
+        <p>Open the menu from the top bar to select operations</p>
       </Paper>
     )
   }
--- src/admin/components/UserList.jsx
+++ src/admin/components/UserList.jsx
@@ -8,46 +8,55 @@ import EditIcon from 'material-ui/lib/svg-icons/editor/mode-edit';
 export default class UserList extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
-    fetch('/user', {credentials: 'same-origin'}).then(response => response.json())
-      .then(json => this.setState({users: json}))
+    this.state = {};
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    fetch('/user', {credentials: 'same-origin'})
+      .then(response => response.json())
+      .then(json => this.setState({users: json}));
   }
 
   handleDelete = (userid) => {
     let url = '/user/' + userid;
-    fetch(url, {method: 'post', credentials: 'same-origin'})    
+    fetch(url, {method: 'post', credentials: 'same-origin'})
+      .then(this.getUsers);
   }
 
   render() {
     const users = this.state.users;
-    if(!users) return <div>loading...</div>
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHeaderColumn>ID</TableHeaderColumn>
-            <TableHeaderColumn>Username</TableHeaderColumn>
-            <TableHeaderColumn>User Type</TableHeaderColumn>
-            <TableHeaderColumn>Edit</TableHeaderColumn>
-            <TableHeaderColumn>Delete</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {users.map(user =>
-            <TableRow key={user.username}>
-              <TableRowColumn>{user._id}</TableRowColumn>
-              <TableRowColumn>{user.username}</TableRowColumn>
-              <TableRowColumn>{user.usertype}</TableRowColumn>
-              <TableRowColumn>
-                  <EditIcon style={{cursor: "pointer"}} hoverColor="#FF0000"/>
-              </TableRowColumn>
-              <TableRowColumn>
-                  <FlatButton label="Delete" onTouchTap={this.handleDelete.bind(null, user._id)}/>
-              </TableRowColumn>
+    // if(!users) return <div>loading...</div>
+    return <div>
+      {!this.state.users ?
+        <div>loading...</div> :
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Username</TableHeaderColumn>
+              <TableHeaderColumn>User Type</TableHeaderColumn>
+              <TableHeaderColumn>Edit</TableHeaderColumn>
+              <TableHeaderColumn>Delete</TableHeaderColumn>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    )
+          </TableHeader>
+          <TableBody>
+            {users.map(user =>
+              <TableRow key={user._id}>
+                <TableRowColumn>{user._id}</TableRowColumn>
+                <TableRowColumn>{user.username}</TableRowColumn>
+                <TableRowColumn>{user.usertype}</TableRowColumn>
+                <TableRowColumn>
+                    <EditIcon style={{cursor: "pointer"}} hoverColor="#FF0000"/>
+                </TableRowColumn>
+                <TableRowColumn>
+                    <FlatButton label="Delete" onTouchTap={this.handleDelete.bind(null, user._id)}/>
+                </TableRowColumn>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      }
+      </div>
   }
 }
