263cc6dd339e232134f9b1ee98deba11b3bc63f5
Jeff Lanzi <jefflanzi@gmail.com>
2016-03-08 14:39:14 -0800

    separate Admin layout component from routing component

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-import Admin from './Admin.jsx';
-import Index from './components/Index';
+import reducer from './reducers';
+import Admin from './Admin';
+import Home from './components/Home';
 import Users from './components/Users';
 import Surveys from './components/Surveys';
 import Charts from './components/Charts';
 
+let store = createStore(reducer);
+
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={Admin}>
-      <IndexRoute component={Index} />
-      <Route path="users" component={Users} />
-      <Route path="surveys" component={Surveys} />
-      <Route path="charts" component={Charts} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={Admin}>
+        <IndexRoute component={Home} />
+        <Route path="users" component={Users} />
+        <Route path="surveys" component={Surveys} />
+        <Route path="charts" component={Charts} />
+      </Route>
+    </Router>
+  </Provider>
 ), document.getElementById('admin'));
